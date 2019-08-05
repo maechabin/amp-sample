137cdc553fde4e3bb49a6c08982af91c66a50ba7
maeda <takanori@stroly.jp>
2019-08-05 21:38:24 +0900

    server対応

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use((req, res, next) => {
+  const host = req.get('host');
+  const protocol = host.startsWith('localhost') ? 'http' : 'https';
+  const origin = protocol + '://' + req.get('host');
+  res.set('Access-Control-Allow-Origin', origin);
+  res.set('AMP-Access-Control-Allow-Source-Origin', origin);
+  res.set(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEAD, PUT');
+  res.set('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+app.use(express.static(__dirname + '/static'));
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
